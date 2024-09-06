2cca63724d158c97d48e545e8fe500b8c7aa33eb
Hirdesh Sharma <121444844+hirdesharma@users.noreply.github.com>
2024-09-06 09:47:03 +0530

    cleaned UserServiceApplication

--- user-service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableCaching
 public class UserServiceApplication {
 
   public static void main(String[] args) {
